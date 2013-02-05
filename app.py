33df111f97c0341ed88973aa0dcc27ad3cb3636f
Brian Munroe <brian.e.munroe@gmail.com>
2013-02-04 21:03:56 -0800

    this comment is so meaningless

--- app.py
+++ app.py
@@ -1,39 +1,14 @@
-import sqlite3
-
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, Response
 
 import requests
 import json
 
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 TASK_SERVICE_URL = 'http://localhost:5000'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.debug = True
-
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-@app.teardown_request
-def teardown_request(exception):
-    g.db.close()
+app.debug = False
 
 @app.route('/')
 def show_dashboard():
@@ -71,5 +46,4 @@ def show_mobile_remote():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-    # app.run()
 
